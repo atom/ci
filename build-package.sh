3bd414e3d6432360a44512d691c426414eb42ab9
Joe Fitzgerald <jfitzgerald@pivotal.io>
2016-03-18 14:56:25 -0600

    Revert #40
    - This is addressed via atom.io instead of the CI script

--- build-package.sh
+++ build-package.sh
@@ -2,10 +2,6 @@
 
 echo "Downloading latest Atom release..."
 ATOM_CHANNEL="${ATOM_CHANNEL:=stable}"
-ACCESS_TOKEN_CHECK="${ATOM_ACCESS_TOKEN:=unset}"
-if [ "$ACCESS_TOKEN_CHECK" = "unset" ]; then
-  export ATOM_ACCESS_TOKEN="da809a6077bb1b0aa7c5623f7b2d5f1fec2faae4"
-fi
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
     curl -s -L "https://atom.io/download/mac?channel=$ATOM_CHANNEL" \
