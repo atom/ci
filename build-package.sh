60d4ad888536482820ac8f88e95e956876ff546f
Landon Abney <Landonabney@gmail.com>
2016-09-12 10:22:05 -0700

    Fix some indentation
    Somehow the major parts of the first `if` statement were using 4 spaces instead of the two the rest of the file used.

--- build-package.sh
+++ build-package.sh
@@ -4,41 +4,41 @@ echo "Downloading latest Atom release..."
 ATOM_CHANNEL="${ATOM_CHANNEL:=stable}"
 
 if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-    curl -s -L "https://atom.io/download/mac?channel=${ATOM_CHANNEL}" \
-      -H 'Accept: application/octet-stream' \
-      -o "atom.zip"
-    mkdir atom
-    unzip -q atom.zip -d atom
-    if [ "${ATOM_CHANNEL}" = "stable" ]; then
-      export ATOM_APP_NAME="Atom.app"
-      export ATOM_SCRIPT_NAME="atom.sh"
-      export ATOM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh"
-    else
-      export ATOM_APP_NAME="Atom ${ATOM_CHANNEL}.app"
-      export ATOM_SCRIPT_NAME="atom-${ATOM_CHANNEL}"
-      export ATOM_SCRIPT_PATH="./atom-${ATOM_CHANNEL}"
-      ln -s "./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh" "${ATOM_SCRIPT_PATH}"
-    fi
-    export ATOM_PATH="./atom"
-    export APM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/apm"
-    export NPM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/npm"
+  curl -s -L "https://atom.io/download/mac?channel=${ATOM_CHANNEL}" \
+    -H 'Accept: application/octet-stream' \
+    -o "atom.zip"
+  mkdir atom
+  unzip -q atom.zip -d atom
+  if [ "${ATOM_CHANNEL}" = "stable" ]; then
+    export ATOM_APP_NAME="Atom.app"
+    export ATOM_SCRIPT_NAME="atom.sh"
+    export ATOM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh"
+  else
+    export ATOM_APP_NAME="Atom ${ATOM_CHANNEL}.app"
+    export ATOM_SCRIPT_NAME="atom-${ATOM_CHANNEL}"
+    export ATOM_SCRIPT_PATH="./atom-${ATOM_CHANNEL}"
+    ln -s "./atom/${ATOM_APP_NAME}/Contents/Resources/app/atom.sh" "${ATOM_SCRIPT_PATH}"
+  fi
+  export ATOM_PATH="./atom"
+  export APM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/apm"
+  export NPM_SCRIPT_PATH="./atom/${ATOM_APP_NAME}/Contents/Resources/app/apm/node_modules/.bin/npm"
 elif [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    curl -s -L "https://atom.io/download/deb?channel=${ATOM_CHANNEL}" \
-      -H 'Accept: application/octet-stream' \
-      -o "atom-amd64.deb"
-    /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-    export DISPLAY=":99"
-    dpkg-deb -x atom-amd64.deb "${HOME}/atom"
-    if [ "${ATOM_CHANNEL}" = "stable" ]; then
-      export ATOM_SCRIPT_NAME="atom"
-      export APM_SCRIPT_NAME="apm"
-    else
-      export ATOM_SCRIPT_NAME="atom-${ATOM_CHANNEL}"
-      export APM_SCRIPT_NAME="apm-${ATOM_CHANNEL}"
-    fi
-    export ATOM_SCRIPT_PATH="${HOME}/atom/usr/bin/${ATOM_SCRIPT_NAME}"
-    export APM_SCRIPT_PATH="${HOME}/atom/usr/bin/${APM_SCRIPT_NAME}"
-    export NPM_SCRIPT_PATH="${HOME}/atom/usr/share/${ATOM_SCRIPT_NAME}/resources/app/apm/node_modules/.bin/npm"
+  curl -s -L "https://atom.io/download/deb?channel=${ATOM_CHANNEL}" \
+    -H 'Accept: application/octet-stream' \
+    -o "atom-amd64.deb"
+  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+  export DISPLAY=":99"
+  dpkg-deb -x atom-amd64.deb "${HOME}/atom"
+  if [ "${ATOM_CHANNEL}" = "stable" ]; then
+    export ATOM_SCRIPT_NAME="atom"
+    export APM_SCRIPT_NAME="apm"
+  else
+    export ATOM_SCRIPT_NAME="atom-${ATOM_CHANNEL}"
+    export APM_SCRIPT_NAME="apm-${ATOM_CHANNEL}"
+  fi
+  export ATOM_SCRIPT_PATH="${HOME}/atom/usr/bin/${ATOM_SCRIPT_NAME}"
+  export APM_SCRIPT_PATH="${HOME}/atom/usr/bin/${APM_SCRIPT_NAME}"
+  export NPM_SCRIPT_PATH="${HOME}/atom/usr/share/${ATOM_SCRIPT_NAME}/resources/app/apm/node_modules/.bin/npm"
 elif [ "$CIRCLECI" = "true" ]; then
   curl -s -L "https://atom.io/download/deb?channel=$ATOM_CHANNEL" \
     -H 'Accept: application/octet-stream' \
